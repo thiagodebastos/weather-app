1a7e6ecc6050539d951fc5c294794ab34bbc4fe9
Thiago de Bastos <thiago@thiagodebastos.com>
2016-06-30 03:53:02 +1000

    implement forecast.io api locally

--- source/js/getWeather.js
+++ source/js/getWeather.js
@@ -1,50 +1,50 @@
 // return ask + query + apikey
-const query = 'http://api.openweathermap.org/data/2.5/weather?'
-const apikey = '3946023bac4a4be22f711eeaa43667cc';
+const query = 'https://api.forecast.io/forecast/'
+const apikey = '68ea1efc5c50ce10dc6410934a9d0983';
 
-http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=3946023bac4a4be22f711eeaa43667cc
-// const getWeather = (() => {
-//   function promisedWeather(url){
-    const getWeather = new Promise((resolve, reject) => {
-      // First, get the local position
-      getPosition.then((position) => {
-        // Once position is retrieved, get local weather
-        // TODO: add possibility of using default data
-      const url = `${query}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apikey}`;
-      fetch(url)
-      .then((response) => response.json())
-      .then((w) => {
+// NOTE: switched from openweathermap as free version doesnt work due to HTTPS issues
+// http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=3946023bac4a4be22f711eeaa43667cc
 
-        // cache results in object
-        const weather = {
-          rain: (w.rain) ? w.rain : 'no rain',
-          tempC: kelvinToCelsius(w.main.temp),
-          tempF: kelvinToFahrenheit(w.main.temp),
-          clouds: w.clouds.all,
-          location: w.name,
-          country: w.sys.country,
-        }
-        resolve(weather);
-        })
-        // possibly break this out into 'fillweather'
-        getWeather
-          .then((result) => {
-            pos = position.coords;
-            console.log(pos.latitude, pos.longitude);
-            weather = result; //cache results
-            appTemp.textContent = `${weather.tempC} degrees`;
-            appLocation.textContent = `${weather.location}, ${weather.country}`;
-            appClouds.textContent = `Cloud cover: ${weather.clouds}%`;
-            appRain.textContent = `Chance of rain: ${weather.rain}`;
+// Now using forecase.io
+// https://api.forecast.io/forecast/68ea1efc5c50ce10dc6410934a9d0983/37.8267,-122.423
 
-            // event listeners for radio buttom
-            appTempC.addEventListener('click', ((event) =>
-            (appTemp.textContent = weather.tempC)))
-            appTempF.addEventListener('click', ((event) =>
-            (appTemp.textContent = weather.tempF)))
-        })
-      })
+const getWeather = new Promise((resolve, reject) => {
+  // First, get the local position
+  getPosition.then((position) => {
+    // Once position is retrieved, get local weather
+    // TODO: add possibility of using default data
+  const url = 'data/london.json';
+  // const url = `${query}${apikey}/${position.coords.latitude},${position.coords.longitude}`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((w) => {
+
+    // cache results in object
+    const weather = {
+      rain: (w.currently.precipProbability) ? w.currently.precipProbability : 'no rain',
+      tempC: `${fahrenheitToCelsius(w.currently.temperature)} degrees`,
+      tempF: `${w.currently.temperature} degrees`,
+      clouds: w.currently.cloudCover,
+      location: w.timezone,
+    }
+    resolve(weather);
     })
-  // }
-//   return promisedWeather;
-// })()
+    // possibly break this out into 'fillweather'
+    getWeather
+      .then((result) => {
+        pos = position.coords;
+        console.log(pos.latitude, pos.longitude);
+        weather = result; //cache results
+        appTemp.textContent = `${weather.tempC}`;
+        appLocation.textContent = `Timezone: ${weather.location}`;
+        appClouds.textContent = `Cloud cover: ${weather.clouds}`;
+        appRain.textContent = `Chance of rain: ${weather.rain}`;
+
+        // event listeners for radio buttom
+        appTempC.addEventListener('click', ((event) =>
+        (appTemp.textContent = weather.tempC)))
+        appTempF.addEventListener('click', ((event) =>
+        (appTemp.textContent = weather.tempF)))
+    })
+  })
+})
--- source/js/helperFunctions.js
+++ source/js/helperFunctions.js
@@ -11,6 +11,13 @@ function kelvinToFahrenheit(K) {
   return Math.round((K * 9/5) - 459.67)
 }
 
+function celsiusToFahrenheit(C){
+  return Math.round((C * 9/5) + 32)
+}
+
+function fahrenheitToCelsius(F){
+  return Math.round((F - 32) * 5/9)
+}
 // Round down a number and truncate to integer
 // round(2.56445) => 3
 function roundNum(num){
